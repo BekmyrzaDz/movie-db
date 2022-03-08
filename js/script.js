327956cdc4507c0eb7f83b5a4b9658fc404f981a
Bekmyrza <msn.10110922@gmail.com>
2022-03-08 11:06:28 +0600

    Удалил все рекламные блоки со страницы. Изменил жанр фильма. Изменил задний фон постера. Динамически сформировал список фильмов и отсортировал их по алфавиту. Добавил нумерацию выведенных фильмов

--- js/script.js
+++ js/script.js
@@ -25,5 +25,48 @@ const movieDB = {
         "Спасатели малибу",
         "Мстители: Финал",
         "Мстители: Война бесконечности"
-    ]
+    ],
+    sortMovies() {
+        return this.movies.sort();
+    },
+    sendMoviesName() {
+        this.movies.forEach((item, index) => {
+            const li = document.createElement('li');
+
+            li.classList.add('promo__interactive-item');
+
+            promoInteractiveList.append(li);
+
+
+            li.innerHTML = `${index + 1}. ${item}
+            <div class="delete"></div>`;
+        });
+    }
 };
+
+const promo = document.querySelector('.promo'),
+    promoAdv = promo.querySelector('.promo__adv'),
+    img = promoAdv.querySelectorAll('img'),
+    promoContent = promo.querySelector('.promo__content'),
+    promoBg = promoContent.querySelector('.promo__bg'),
+    promoDiv = promoBg.querySelectorAll('div'),
+    promoInteractive = document.querySelector('.promo__interactive'),
+    promoInteractiveList = promoInteractive.querySelector('.promo__interactive-list'),
+    promoInteractiveItem = promoInteractiveList.querySelectorAll('.promo__interactive-item');
+
+img.forEach(item => item.remove());
+
+const div = document.createElement('div');
+
+div.classList.add('promo__genre');
+promoBg.prepend(div);
+
+div.innerHTML = 'драма'.toUpperCase();
+promoDiv[1].remove();
+
+promoBg.style.backgroundImage = 'url("../project/img/bg.jpg")';
+
+promoInteractiveItem.forEach(item => item.remove());
+
+movieDB.sortMovies();
+movieDB.sendMoviesName();
