50d49fc3d9ed5058561d0ddcad2363f271057812
Bekmyrza <msn.10110922@gmail.com>
2022-03-09 15:52:25 +0600

    решение

--- js/script.js
+++ js/script.js
@@ -26,47 +26,76 @@ const movieDB = {
         "Мстители: Финал",
         "Мстители: Война бесконечности"
     ],
-    sortMovies() {
-        return this.movies.sort();
-    },
-    sendMoviesName() {
-        this.movies.forEach((item, index) => {
-            const li = document.createElement('li');
+    // Мой вариант
+    // sortMovies() {
+    //     return this.movies.sort();
+    // },
+    // sendMoviesName() {
+    //     this.movies.forEach((item, index) => {
+    //         const li = document.createElement('li');
 
-            li.classList.add('promo__interactive-item');
+    //         li.classList.add('promo__interactive-item');
 
-            promoInteractiveList.append(li);
+    //         promoInteractiveList.append(li);
 
-
-            li.innerHTML = `${index + 1}. ${item}
-            <div class="delete"></div>`;
-        });
-    }
+    //         li.innerHTML = `${index + 1}. ${item}
+    //         <div class="delete"></div>`;
+    //     });
+    // }
 };
 
-const promo = document.querySelector('.promo'),
-    promoAdv = promo.querySelector('.promo__adv'),
-    img = promoAdv.querySelectorAll('img'),
-    promoContent = promo.querySelector('.promo__content'),
-    promoBg = promoContent.querySelector('.promo__bg'),
-    promoDiv = promoBg.querySelectorAll('div'),
-    promoInteractive = document.querySelector('.promo__interactive'),
-    promoInteractiveList = promoInteractive.querySelector('.promo__interactive-list'),
-    promoInteractiveItem = promoInteractiveList.querySelectorAll('.promo__interactive-item');
+// const promo = document.querySelector('.promo'),
+//     promoAdv = promo.querySelector('.promo__adv'),
+//     img = promoAdv.querySelectorAll('img'),
+//     promoContent = promo.querySelector('.promo__content'),
+//     promoBg = promoContent.querySelector('.promo__bg'),
+//     promoDiv = promoBg.querySelectorAll('div'),
+//     promoInteractive = document.querySelector('.promo__interactive'),
+//     promoInteractiveList = promoInteractive.querySelector('.promo__interactive-list'),
+//     promoInteractiveItem = promoInteractiveList.querySelectorAll('.promo__interactive-item');
 
-img.forEach(item => item.remove());
+// img.forEach(item => item.remove());
 
-const div = document.createElement('div');
+// const div = document.createElement('div');
 
-div.classList.add('promo__genre');
-promoBg.prepend(div);
+// div.classList.add('promo__genre');
+// promoBg.prepend(div);
 
-div.innerHTML = 'драма'.toUpperCase();
-promoDiv[1].remove();
+// div.innerHTML = 'драма'.toUpperCase();
+// promoDiv[1].remove();
 
-promoBg.style.backgroundImage = 'url("../project/img/bg.jpg")';
+// promoBg.style.backgroundImage = 'url("../project/img/bg.jpg")';
 
-promoInteractiveItem.forEach(item => item.remove());
+// promoInteractiveItem.forEach(item => item.remove());
 
-movieDB.sortMovies();
-movieDB.sendMoviesName();
+// movieDB.sortMovies();
+// movieDB.sendMoviesName();
+
+// Решение
+const adv = document.querySelectorAll('.promo__adv img'),
+    poster = document.querySelector('.promo__bg'),
+    genre = poster.querySelector('.promo__genre'),
+    movieList = document.querySelector('.promo__interactive-list');
+
+adv.forEach(item => {
+    item.remove();
+});
+
+genre.textContent = 'драма';
+
+poster.style.backgroundImage = 'url("img/bg.jpg")';
+
+// Пустые кавычки можно использовать для очистки элементов 
+movieList.innerHTML = '';
+
+movieDB.movies.sort();
+
+// console.log(poster.innerHTML);
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+        <li class="promo__interactive-item">${i + 1}. ${film}
+            <div class="delete"></div>
+        </li>
+    `;
+});
